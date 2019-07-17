00a7c7a051f7647ca8f5f3c9c1d0c9195bacc016
Song Hae <thdgo456@naver.com>
2019-07-17 17:07:40 +0900

    Action 클래스를 이용한간단한 로그인 로직 (depth와 result.name 이용)

--- struts02/src/com/bit/action/Ex03Action.java
+++ struts02/src/com/bit/action/Ex03Action.java
@@ -8,7 +8,7 @@ public class Ex03Action implements Action {
 	Logger log = Logger.getLogger(this.getClass());
 	String id;
 	
-	//형변환한 값을 가져올 수 있음. intercepter.Parametersinterceptor 클래스에서 해줌
+	//형변환한 값을 가져올 수 있음. ㅁㅊ 클래스에서 해줌
 	//인터셉터를 통해서 파라미터 형변환을 처리하는 필터를 중간에 넣음
 	//before에서 set000으로 한번, view로 위임하면서 get000으로 한번 해서 총 2번 거치게 됨
 	int num;	
