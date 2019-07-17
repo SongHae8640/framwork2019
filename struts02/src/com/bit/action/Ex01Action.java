371f4f2800ca43d30602dcf757dc35db7a7cb473
Song Hae <thdgo456@naver.com>
2019-07-17 15:38:35 +0900

    Action 클래스의 return인 String에 사용

--- struts02/src/com/bit/action/Ex01Action.java
+++ struts02/src/com/bit/action/Ex01Action.java
@@ -10,6 +10,9 @@ import com.opensymphony.xwork2.Action;
  * 문제점 1. Controller가 excute를 통해 실행 되더라도 인자가 없다.
  * 문제점 2. 받아온 정보를 처리하더라도 보낼 방법을 모른다.
  * 문제점 3. return 자료형으로 String을 사용하는데 이 정보가 View로 가는 것이 아니다.
+ *	-> result의 name값
+ *		아무 String이나 다 되는게 아니라 몇가지 정해진 String을 사용. 
+ *			error, input, login, none, success( Actino에 정의되어 있기 때문에 this.을 통해서도 접근 가능)
  */
 
 
@@ -19,7 +22,9 @@ public class Ex01Action implements Action {
 	@Override
 	public String execute() throws Exception {
 		log.debug("호출");
-		return "success";
+		
+//		return "input";
+		return this.INPUT;
 	}
 
 }
---
+++ struts02/webapp/form.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>입력 페이지</h1>
+	<form>
+		<div>
+			<label for="id">ID</label>
+			<input type= "text" name="id">
+		</div>
+		<div>
+			<button type="submit">전달</button>
+		</div>
+	</form>
+</body>
+</html>
