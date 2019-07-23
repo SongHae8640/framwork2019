fe257c5bf3ff8ad578e908576b0e1b37e2ab912e
Song Hae <thdgo456@naver.com>
2019-07-23 13:09:54 +0900

    servelt init을 이용한 성능향상

--- bitFramework01/src/com/bit/framework/DispatcherServlet.java
+++ bitFramework01/src/com/bit/framework/DispatcherServlet.java
@@ -16,8 +16,21 @@ import com.bit.controller.Controller;
 /*
  * struts에서 filter에서 사용하던것과 같은 기능
  * */
-public class DispatcherServlet extends HttpServlet{
+public class DispatcherServlet extends HttpServlet {
+	Map<String, String> map = new HashMap<String, String>();
+	
+	//mapping 관리를 init에서 선언
+	//container가 생성될때 한번만 
+	///컨트롤러 또한 를 매번 찍지 않고 init 시점에 생성하고 이후에 호출하는 방식으로 가능(이후에는 파일로 내용을 관리)
+	public void init() throws ServletException {
+		map.put("/index.bit", "com.bit.controller.IndexController");
+		map.put("/main.bit", "com.bit.controller.MainController");
+		map.put("/list.bit", "com.bit.controller.ListController");
+		map.put("/add.bit", "com.bit.controller.AddController");
+		map.put("/insert.bit", "com.bit.controller.InsertController");
 
+	};
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
@@ -58,12 +71,6 @@ public class DispatcherServlet extends HttpServlet{
 		String suffix = ".jsp";
 		com.bit.controller.Controller controller = null;
 		String clInfo ="";
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("/index.bit", "com.bit.controller.IndexController");
-		map.put("/main.bit", "com.bit.controller.MainController");
-		map.put("/list.bit", "com.bit.controller.ListController");
-		map.put("/add.bit", "com.bit.controller.AddController");
-		map.put("/insert.bit", "com.bit.controller.InsertController");
 		
 		
 		
